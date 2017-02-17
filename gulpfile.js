23d5ca5d4a53c39e938b97aaf5138db3915b866a
Drew Barontini <drewbarontini@gmail.com>
2017-02-16 20:29:20 -0500

    Add gulp server and watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var connect = require('gulp-connect');
 
 gulp.task('build', function() {
   return gulp.src('src/app.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('public'));
+});
+
+gulp.task('server', function() {
+  gulp.watch(['src/app.css'], ['build']);
+  connect.server();
 });
