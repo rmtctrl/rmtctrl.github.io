b4c1075fb382284ebfb5d20031e564e7491e243c
Drew Barontini <drewbarontini@gmail.com>
2017-02-18 10:59:31 -0500

    Update hero content animation

--- public/app.js
+++ public/app.js
@@ -1,1 +1,1 @@
-function handleImageLoad(e){setTimeout(function(){hero.classList.add("is-loaded")},500)}var image=document.createElement("img"),hero=document.querySelector(".hero");image.src="public/bg-hero.jpg",image.addEventListener("load",handleImageLoad);
+function handleImageLoad(e){setTimeout(function(){document.body.classList.add("is-hero-loaded")},500)}var image=document.createElement("img");image.src="public/bg-hero.jpg",image.addEventListener("load",handleImageLoad);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 var image = document.createElement('img');
-var hero = document.querySelector('.hero');
 
 image.src = 'public/bg-hero.jpg';
 
 function handleImageLoad(event) {
   setTimeout(function() {
-    hero.classList.add('is-loaded');
+    document.body.classList.add('is-hero-loaded');
   }, 500);
 }
 
